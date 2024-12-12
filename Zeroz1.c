c70a9dc5fe2c76a6fe27e6fc13ceff063a253e98
Vikram-zeroz <zerozworldofficial@gmail.com>
2024-12-12 03:30:50 -0800

    Zeroz1.c

--- Zeroz1.c
+++ Zeroz1.c
@@ -3,8 +3,12 @@
 int main ()
 {
     char  str[20];
+    char  str2[10];
     printf("enter your name :");
     fgets(str,sizeof(str),stdin);
+    printf("enter your Age :");
+    fgets(str2,sizeof(str2),stdin);
     printf("Your name is : %s",str);
+    printf("Your age is : %s",str2);
 
 }
